9ef2ce5bd1fef7531edf9563830e14644cd754f3
hendi <hendi@xulu.co.id>
2021-11-08 23:10:43 +0700

    refactor:
    test link message
    
    resolved #https://github.com/Hendi-arch/flutter-simple-state-management/tree/main/simple-movie-getx

--- simple-movie-mobx/lib/app/app_router.gr.dart
+++ simple-movie-mobx/lib/app/app_router.gr.dart
@@ -1,53 +1,58 @@
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 // **************************************************************************
 // AutoRouteGenerator
 // **************************************************************************
 
-import 'package:auto_route/auto_route.dart' as _i1;
-import 'package:flutter/material.dart' as _i2;
+import 'package:auto_route/auto_route.dart' as _i3;
+import 'package:flutter/material.dart' as _i4;
 import 'package:movie_app_test/core/constant/enums.dart' as _i5;
 import 'package:movie_app_test/core/models/movie_model.dart' as _i6;
 import 'package:movie_app_test/views/inside/movie/content/movie_detail.dart'
-    as _i4;
-import 'package:movie_app_test/views/inside/movie/movie_view.dart' as _i3;
+    as _i2;
+import 'package:movie_app_test/views/inside/movie/movie_view.dart' as _i1;
 
-class AppRouter extends _i1.RootStackRouter {
-  AppRouter([_i2.GlobalKey<_i2.NavigatorState>? navigatorKey])
+class AppRouter extends _i3.RootStackRouter {
+  AppRouter([_i4.GlobalKey<_i4.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i1.PageFactory> pagesMap = {
-    MovieViewRoute.name: (routeData) => _i1.AdaptivePage<dynamic>(
-        routeData: routeData,
-        builder: (_) {
-          return const _i3.MovieView();
-        }),
-    MovieDetailRoute.name: (routeData) => _i1.AdaptivePage<dynamic>(
-        routeData: routeData,
-        builder: (data) {
-          final args = data.argsAs<MovieDetailRouteArgs>();
-          return _i4.MovieDetail(
-              key: args.key, appState: args.appState, data: args.data);
-        })
+  final Map<String, _i3.PageFactory> pagesMap = {
+    MovieViewRoute.name: (routeData) {
+      return _i3.AdaptivePage<dynamic>(
+          routeData: routeData, child: const _i1.MovieView());
+    },
+    MovieDetailRoute.name: (routeData) {
+      final args = routeData.argsAs<MovieDetailRouteArgs>();
+      return _i3.AdaptivePage<dynamic>(
+          routeData: routeData,
+          child: _i2.MovieDetail(
+              key: args.key, appState: args.appState, data: args.data));
+    }
   };
 
   @override
-  List<_i1.RouteConfig> get routes => [
-        _i1.RouteConfig(MovieViewRoute.name, path: '/'),
-        _i1.RouteConfig(MovieDetailRoute.name, path: 'movie_detail')
+  List<_i3.RouteConfig> get routes => [
+        _i3.RouteConfig(MovieViewRoute.name, path: '/'),
+        _i3.RouteConfig(MovieDetailRoute.name, path: 'movie_detail')
       ];
 }
 
-class MovieViewRoute extends _i1.PageRouteInfo {
+/// generated route for [_i1.MovieView]
+class MovieViewRoute extends _i3.PageRouteInfo<void> {
   const MovieViewRoute() : super(name, path: '/');
 
   static const String name = 'MovieViewRoute';
 }
 
-class MovieDetailRoute extends _i1.PageRouteInfo<MovieDetailRouteArgs> {
+/// generated route for [_i2.MovieDetail]
+class MovieDetailRoute extends _i3.PageRouteInfo<MovieDetailRouteArgs> {
   MovieDetailRoute(
-      {_i2.Key? key, required _i5.AppState appState, _i6.MovieModel? data})
+      {_i4.Key? key, required _i5.AppState appState, _i6.MovieModel? data})
       : super(name,
             path: 'movie_detail',
             args:
@@ -59,7 +64,7 @@ class MovieDetailRoute extends _i1.PageRouteInfo<MovieDetailRouteArgs> {
 class MovieDetailRouteArgs {
   const MovieDetailRouteArgs({this.key, required this.appState, this.data});
 
-  final _i2.Key? key;
+  final _i4.Key? key;
 
   final _i5.AppState appState;
 
